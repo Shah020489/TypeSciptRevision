9af4bffefb2fd911f7ad4d82315ecad47cd701bf
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-05-30 16:57:52 +0500

    foor loop file is added in it.

---
+++ forloop.js
@@ -0,0 +1,12 @@
+/** For Loop */
+/**
+for(let i=1;i<10;++i){
+    console.log(i);
+}
+*/
+// for (let i=5;i<=25;i=i+2){
+//     console.log(i);
+// }
+for (var a = 5; a > 1; a--) {
+    console.log(a);
+}
---
+++ forloop.ts
@@ -0,0 +1,17 @@
+
+/** For Loop */
+/** 
+for(let i=1;i<10;++i){
+    console.log(i);
+}
+*/
+
+// for (let i=5;i<=25;i=i+2){
+//     console.log(i);
+
+// }
+
+
+for(let a=5;a>1;a--){
+    console.log(a);
+}
