344db22f52dc379b7170e8993bf2df571c76e0fb
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-05-30 17:03:02 +0500

    whileLoopIsAdded.

---
+++ whileLoop.js
@@ -0,0 +1,11 @@
+/**** While Loop  */
+// let a=5;
+// while(a>1){
+//     console.log(a);
+//     a--;
+// }
+var abc = 1;
+while (abc <= 10) {
+    console.log(abc);
+    abc = abc + 2;
+}
---
+++ whileLoop.ts
@@ -0,0 +1,13 @@
+/**** While Loop  */
+
+// let a=5;
+// while(a>1){
+//     console.log(a);
+//     a--;
+// }
+
+let abc=1;
+while(abc<=10){
+    console.log(abc);
+    abc=abc+2;
+}
